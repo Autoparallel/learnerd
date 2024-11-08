52c4d968df8d07b269e10d9a993651431c71c463
Colin Roberts <colin@primitive.xyz>
2024-11-07 19:04:58 -0700

    WIP: continuing linux daemon

--- crates/learnerd/src/daemon.rs
+++ crates/learnerd/src/daemon.rs
@@ -228,6 +228,11 @@ Type=simple
 User=root
 Group=root
 ExecStart=/usr/local/bin/learnerd daemon start
+Restart=on-failure
+RestartSec=60
+
+# Keep the service running
+RemainAfterExit=yes
 
 # Let systemd handle logging directly
 StandardOutput=journal
